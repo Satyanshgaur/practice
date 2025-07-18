7d8b58664716de2a4833d1b53b0b62d4f412dc84
Satyansh Gaur <satyanshgaur2005@gmail.com>
2025-07-18 15:45:50 +0530

    git push --force Resolved merge conflict in yolo.py

--- yolo.py
+++ yolo.py
@@ -1,5 +1,5 @@
 # NumPy routines which allocate memory and fill arrays with value
 import numpy as np
-a = np.zeros(4);                print(f"np.zeros(4) :   a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
-a = np.zeros((4,));             print(f"np.zeros(4,) :  a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
-a = np.random.random_sample(4); print(f"np.random.random_sample(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+
+a = np.arange(4.);              print(f"np.arange(4.):     a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.random.rand(4);          print(f"np.random.rand(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
